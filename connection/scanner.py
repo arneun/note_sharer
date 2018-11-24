d6d3c322f9ba9e7cdde246abbb3b680116f97040
Arneun <arneun@Karmagedon.home>
2018-11-24 02:13:54 +0100

    Netscan with netifaces, fix to aquire network ip still needed

--- connection/scanner.py
+++ connection/scanner.py
@@ -1,23 +1,34 @@
 
 import socket
-import request
 import asyncio
 import ipaddress
+import netifaces
 
 class Scanner:
 
-    def getIP(self):
-        myIP = ipaddress.ip_address( socket.gethostbyname(socket.gethostname()) )
+    def get_ip(self):
+        myIP = ('','')
+        interfaces = netifaces.interfaces()
+        for i in interfaces:
+            if i == 'lo':
+                continue
+            iface = netifaces.ifaddresses(i).get(netifaces.AF_INET)
+            if iface != None:
+                for j in iface:
+                    myIP = (j['addr'], j['netmask'])
         return myIP
     
     
-    def addresses_in_subnet(self, host_ip):
-       return ipaddress.ip_network(host_ip)
+    def addresses_in_subnet(self, host_ip, mask):
+        
+        return ipaddress.ip_network(host_ip + '/' + mask)
 
 
 
     def scan_all(self):
-        
+        (ip, mask) = get_ip()
+        adresses = addresses_in_subnet(ip, mask)
+        adresses.remove(ip)
         
 
 
