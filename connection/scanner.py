fa8020461f3ea65722cccbf36e2e01ed993f18e5
Arneun <arneun@Karmagedon.home>
2018-12-02 20:07:49 +0100

    async ip scan, first idea

--- connection/scanner.py
+++ connection/scanner.py
@@ -30,15 +30,17 @@ class Scanner:
 
 
     def scan_all(self):
+        loop = asyncio.get_event_loop()
         (ip, mask) = get_ip()
         adresses = addresses_in_subnet(__get_net_addr(ip, mask), mask)
         adresses.remove(ip)
-        checks = [check() ]
+        checks = [ip for ip in adresses if self.check(ip)]
+        return loop.run_until_complete(asyncio.gather(checks))
 
     async def check(self, ip_addr):
         r = requests.get('ip_addr' + ':5000/greet')        
         if r.text == 'hey':
-            return ip_addr True
+            return True
         else: 
             return False
 
